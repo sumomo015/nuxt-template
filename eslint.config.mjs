0bbb8dd817dd3d894175f81b6ef08d6f268dca0d
sumomo015 <138793740+sumomo015@users.noreply.github.com>
2024-08-24 21:48:58 +0900

    chore: Update eslint configuration for TypeScript and Vue files

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,43 @@
+// @ts-check
+
+import vueParser from 'vue-eslint-parser'
+import tsParser from '@typescript-eslint/parser'
+import tseslint from 'typescript-eslint'
+
 import withNuxt from './.nuxt/eslint.config.mjs'
 
-export default withNuxt(
-).override('nuxt/typescript/rules', {
-  rules: {
-    '@typescript-eslint/no-non-null-assertion': 'error',
-  },
-}).override('nuxt/javascript', {
-  rules: {
-    'no-console': 'warn',
-  },
-})
+export default withNuxt()
+  .remove('nuxt/typescript/setup')
+  .replace('nuxt/typescript/rules',
+    // @ts-ignore
+    ...tseslint
+      .config(
+        ...tseslint.configs.strictTypeChecked,
+        ...tseslint.configs.stylisticTypeChecked,
+      )
+      .map(v => ({
+        ...v,
+        files: ['**/*.ts', '**/*.vue'],
+      })),
+    {
+      files: ['**/*.ts', '**/*.vue'],
+      rules: {
+        'no-console': 'warn',
+        '@typescript-eslint/restrict-template-expressions': [
+          'error', { allowNumber: true },
+        ],
+      },
+    })
+  .override('nuxt/vue/setup', {
+    files: ['**/*.vue', '**/*.ts'],
+    languageOptions: {
+      parser: vueParser,
+      parserOptions: {
+        parser: tsParser,
+        sourceType: 'module',
+        extraFileExtensions: ['.vue'],
+        project: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  })
